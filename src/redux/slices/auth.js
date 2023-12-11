c17be40e6058a72885cb54ad64195fd6fbadd4f9
ThienNhan16 <110485158+ThienNhan16@users.noreply.github.com>
2023-12-11 20:48:50 +0700

    Update auth.js

--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -258,7 +258,7 @@ export function VerifyEmail(formValues) {
         );
       })
       .finally(() => {
-          if (getState().auth.success) {
+          if (!getState().auth.error) {
             window.location.href = "/auth/login";
           }
       });
